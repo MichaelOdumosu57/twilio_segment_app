8e31eb378fe49bff7a1779671ac9a1749f09d5dc
Restop-2345 <michaelodumosu57@gmail.com>
2021-08-31 17:31:21 -0400

    [UPDATE]

--- AngularApp/src/environments/environment.ts
+++ AngularApp/src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
     startURL:"/",
     googleMaps:{
         apiKey:"AIzaSyBGRcFDS9q6DjjutDkQlshOItikFxnYjIs"
+        
     },
     backend:{
         url: "http://127.0.0.1:5000",
