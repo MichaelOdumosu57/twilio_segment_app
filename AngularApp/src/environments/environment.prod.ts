49ad0ba7e526f395f22cc482061b4c1efcc23f0e
Restop-2345 <michaelodumosu57@gmail.com>
2021-09-01 09:12:00 -0400

    [UPDATE] updated backend prod url

--- AngularApp/src/environments/environment.prod.ts
+++ AngularApp/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
         apiKey:"AIzaSyBGRcFDS9q6DjjutDkQlshOItikFxnYjIs"
     },
     backend:{
-        url: "http://127.0.0.1:5000",
+        url: "https://twilio-segment-app.herokuapp.com",
     },
     logging:{
         url:"http://127.0.0.1:6111"
